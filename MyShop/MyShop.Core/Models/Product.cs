068fd0ca62a10557d227d99b9e5d5bf226d74fd1
Rohit Kumar Verma <rverma@aakashinformatics.com>
2023-02-17 17:30:30 +0530

    Product Models and InMemory Product Repository Built

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [StringLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        [Required]
+        public string Description { get; set; }
+        [DisplayName("Price")]
+        [Required]
+        [Range(0, 1500)]
+        public string Price { get; set; }
+        [DisplayName("Category")]
+        [Required]
+        [StringLength(25)]
+        public string Category { get; set; }
+        [DisplayName("Image")]
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+
+        }
+    }
+}
